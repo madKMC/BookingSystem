6dad6fbfab2ac6fdae0ce8dc8279ad9a557e43b9
Kaylen Cairns <kaylen.cairns@gmail.com>
2025-07-31 11:56:01 +0200

    Removed backend as it was not needed, Added google calendar scheduling embedded into BookingPage.tsx

--- booking-system/src/pages/BookingPage.tsx
+++ booking-system/src/pages/BookingPage.tsx
@@ -1,6 +1,6 @@
 import Header from '../components/Header.tsx';
 import Footer from '../components/Footer.tsx';
-import Booking from '../components/Booking.tsx';
+// import Booking from '../components/Booking.tsx';
 
 const BookingPage = () => {
 	return (
@@ -8,7 +8,13 @@ const BookingPage = () => {
 			<div className='min-h-screen flex flex-col'>
 				<Header></Header>
 				<main className='flex-1'>
-					<Booking />
+					{/* <Booking /> */}
+					{/* <!-- Google Calendar Appointment Scheduling begin --> */}
+					<iframe
+						src='https://calendar.google.com/calendar/appointments/schedules/AcZssZ3XqpKQ6MXC8s2uj_BN2CBRDAdzmPgw59TyH9yUxrVwVKbvvXpZnZ_HllVhQGPA2NG592UkgwqJ?gv=true'
+						className={'w-full h-screen border-0'}
+					></iframe>
+					{/* <!-- end Google Calendar Appointment Scheduling --> */}
 				</main>
 				<Footer />
 			</div>
